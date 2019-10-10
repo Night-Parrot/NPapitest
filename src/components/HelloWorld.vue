97c5e25f68c5a95c2a78da338db5621a56e972d5
Night-Parrot <youdewo@163.com>
2019-10-11 00:11:25 +0800

    主页修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,166 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <a-table :dataSource="data" :columns="columns" :pagination="pagination">
+    <div slot="filterDropdown" slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }" class='custom-filter-dropdown'>
+      <a-input
+        v-ant-ref="c => searchInput = c"
+        :placeholder="`请输入${column.title}`"
+        :value="selectedKeys[0]"
+        @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+        @pressEnter="() => handleSearch(selectedKeys, confirm)"
+        style="width: 188px; margin-bottom: 8px; display: block;"
+      />
+      <a-button
+        type='primary'
+        @click="() => handleSearch(selectedKeys, confirm)"
+        icon="search"
+        size="small"
+        style="width: 90px; margin-right: 8px"
+      >查询</a-button>
+      <a-button
+        @click="() => handleReset(clearFilters)"
+        size="small"
+        icon="reload"
+        style="width: 90px"
+      >清空</a-button>
+    </div>
+    <a-icon slot="filterIcon" slot-scope="filtered" type='search' :style="{ color: filtered ? '#108ee9' : undefined }" />
+    <template slot="customRender" slot-scope="text">
+      <span v-if="searchText">
+        <template v-for="(fragment, i) in text.toString().split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))">
+          <mark v-if="fragment.toLowerCase() === searchText.toLowerCase()" :key="i" class="highlight">{{fragment}}</mark>
+          <template v-else>{{fragment}}</template>
+        </template>
+      </span>
+      <template v-else>{{text}}</template>
+    </template>
+  </a-table>
 </template>
 
 <script>
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  name: 'Joe Black',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Jim Green',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}, {
+  key: '4',
+  name: 'Jim Red',
+  age: 32,
+  address: 'London No. 2 Lake Park',
+},{
+  key: '5',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '6',
+  name: 'Joe Black',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '7',
+  name: 'Jim Green',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}, {
+  key: '8',
+  name: 'Jim Red',
+  age: 32,
+  address: 'London No. 2 Lake Park',
+}, {
+  key: '9',
+  name: 'Jim Red',
+  age: 32,
+  address: 'London No. 2 Lake Park',
+},{
+  key: '10',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '11',
+  name: 'Joe Black',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '12',
+  name: 'Jim Green',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}, {
+  key: '13',
+  name: 'Jim Red',
+  age: 32,
+  address: 'London No. 2 Lake Park',
+}]
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data () {
+    return {
+      data,
+      searchText: '',
+      searchInput: null,
+      columns: [{
+        title: '项目名称',
+        dataIndex: 'name',
+        key: 'name',
+        align: 'center',
+        width: '200',
+        scopedSlots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender',
+        },
+        onFilter: (value, record) => record.name.toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible) => {
+          if (visible) {
+            setTimeout(() => {
+              this.searchInput.focus()
+            },0)
+          }
+        },
+        pagination: {
+              hideOnSinglePage: true,
+              total: 500,
+            }
+      }],
+    }
+  },
+  methods: {
+    handleSearch (selectedKeys, confirm) {
+      confirm()
+      this.searchText = selectedKeys[0]
+    },
+
+    handleReset (clearFilters) {
+      clearFilters()
+      this.searchText = ''
+    },
+
+
+  },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.custom-filter-dropdown {
+  padding: 8px;
+  border-radius: 4px;
+  background: #fff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .15);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.highlight {
+  background-color: rgb(255, 192, 105);
+  padding: 0px;
 }
 </style>
