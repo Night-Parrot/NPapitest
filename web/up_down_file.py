1fd77cf988c0494400a3b63a4a831675d35e11f0
Night-Parrot <youdewo@163.com>
2020-04-19 18:38:18 +0800

    添加用例模板的下载接口

--- web/up_down_file.py
+++ web/up_down_file.py
@@ -7,6 +7,7 @@ from flask import Blueprint, Flask, abort, jsonify, request, send_from_directory
 import math
 from flask_uploads import UploadSet
 from flask_uploads import configure_uploads, patch_request_class
+from flask_login import login_required
 
 from common import my_log, panda_for_web, base_tool
 
@@ -27,18 +28,19 @@ from main import app
 
 @asyncio.coroutine
 @main.route('/uploadfile', methods=['post'])
+@login_required
 def uploadfile(): #上传文件
     if 'file' not in request.files or 'xmdz' not in request.form:
         abort(400)
     xmdz = request.form.get('xmdz')
     file = request.files.get('file')
-    sql_xm_bh = "SELECT c_bh FROM db_apitesting.t_at_xmxx WHERE c_url = '%s';" % (xmdz)
+    sql_xm_bh = "SELECT c_bh, c_code_team AS code, c_test_team AS test FROM db_apitesting.t_at_xmxx WHERE c_url = '%s';" % (xmdz)
     try:
-        xmbh = all_dbc.pg_select_operator(sql_xm_bh)
-        if len(xmbh) == 0:
+        xm_info = all_dbc.pg_select_operator(sql_xm_bh)
+        if len(xm_info) == 0:
             return jsonify({'result': 'fail', 'msg': '项目不存在'})
         else:
-            xmbh = xmbh[0]['c_bh']
+            xmbh = xm_info[0]['c_bh']
     except Exception as eee:
         logger.error('根据项目地址查询项目编号失败' + str(eee))
         return jsonify({'result': 'fail', 'msg': str(eee)})
@@ -62,8 +64,8 @@ def uploadfile(): #上传文件
         except Exception as eee:
             return jsonify({'result': 'fail', 'msg': '解析参数失败，请检查参数sheet页'})
         ylbh = base_tool.next_id()
-        sql_addyl = "INSERT INTO db_apitesting.t_at_ylxx(c_bh, c_bh_xm, c_ylmc, dt_scsj, dt_zxzxsj, n_zxcs, c_bclj) "\
-                    "VALUES ('%s', '%s', '%s', now(), NULL, 0, '%s');" % (ylbh, xmbh, file_name, newpath_all)
+        sql_addyl = "INSERT INTO db_apitesting.t_at_ylxx(c_bh, c_bh_xm, c_ylmc, dt_scsj, dt_zxzxsj, n_zxcs, c_bclj, c_edit_key, c_sfbj, dt_gxsj, c_code_team, c_test_team) "\
+                    "VALUES ('%s', '%s', '%s', now(), NULL, 0, '%s', '%s', 2, now(), '%s', '%s');" % (ylbh, xmbh, file_name, newpath_all, ylbh, xm_info[0]['code'], xm_info[0]['test'])
         try:
             all_dbc.pg_insert_operator(sql_addyl)
         except Exception as eee:
@@ -87,6 +89,7 @@ def uploadfile(): #上传文件
 
 @asyncio.coroutine
 @main.route('/updatefile', methods=['post'])
+@login_required
 def updatefile(): #上传文件
     if 'file' not in request.files or 'ylbh' not in request.form:
         abort(400)
@@ -149,7 +152,8 @@ def updatefile(): #上传文件
                 return jsonify({'result': 'fail', 'msg': '参数插入失败'})
             intnum = intnum + 1
         try:
-            sql_addyl = "UPDATE db_apitesting.t_at_ylxx SET c_bclj = '%s' WHERE c_bh = '%s'" % (newpath_all, ylbh)
+            new_edit_key = base_tool.next_id()
+            sql_addyl = "UPDATE db_apitesting.t_at_ylxx SET c_bclj = '%s', c_edit_key = '%s', dt_gxsj = now() WHERE c_bh = '%s'" % (newpath_all, new_edit_key, ylbh, )
             all_dbc.pg_insert_operator(sql_addyl)
         except Exception as eee:
             logger.error('插入用例信息错误：' + str(eee))
@@ -163,11 +167,11 @@ def updatefile(): #上传文件
 
 
 
-
 @asyncio.coroutine
 @main.route('/downloadfile/<ylbh>', methods=['get'])
 def downloadfile(ylbh): #下载文件
     sql_filepath = "SELECT c_bclj as lj,c_ylmc as mc FROM db_apitesting.t_at_ylxx WHERE c_bh = '%s';" % ylbh
+    logger.info(sql_filepath)
     try:
         filepath = all_dbc.pg_select_operator(sql_filepath)
         new_filepath = filepath[0]['lj']
@@ -209,3 +213,20 @@ def downloadyhsc(): #下载用户手册
         return jsonify({'result': 'fail', 'msg': str(eee)})
 
 
+
+@asyncio.coroutine
+@main.route('/downloadkbyl', methods=['get'])
+def downloadkbyl(): #下载空白用例
+    try:
+        logger.info(os.getcwd())
+        filepath = os.getcwd() + '/upload/用例模板.xlsx'
+        logger.error(filepath)
+        p, f = os.path.split(filepath)
+        if os.path.isfile(filepath):
+            response = make_response(send_from_directory(p, f, as_attachment=True))
+            return response
+        else:
+            return jsonify({'result': 'fail', 'msg': '文件不存在'})
+    except Exception as eee:
+        logger.error(eee)
+        return jsonify({'result': 'fail', 'msg': str(eee)})
