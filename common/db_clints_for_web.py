0385134b9609636bfde292f369c21fce36bc8a5e
Night-Parrot <youdewo@163.com>
2020-04-19 18:21:52 +0800

    添加处理查询结果有大写字母的情况

--- common/db_clints_for_web.py
+++ common/db_clints_for_web.py
@@ -11,6 +11,32 @@ logger = my_log.LogUtil().getLogger()
 
 
 
+# def db_clints(db_info):
+#     db_clints = {}
+#     if len(db_info) == 0:
+#         logger.info('用例中未读取到数据库配置信息')
+#         return False 
+#     for db_clint in db_info:
+#         try:
+#             database = db_info[db_clint]['db_name']
+#             user = db_info[db_clint]['db_user']
+#             password = db_info[db_clint]['db_password']
+#             host = db_info[db_clint]['db_ip']
+#             port = db_info[db_clint]['db_port']
+#             db_fun = []
+#             build_db_for_commit = psycopg2.connect(
+#                 database=database, user=user, password=password, host=host, port=port)
+#             db_fun.append(build_db_for_commit)
+#             build_db_for_cur = build_db_for_commit.cursor(
+#                 cursor_factory=psycopg2.extras.RealDictCursor)
+#             db_fun.append(build_db_for_cur)
+#             db_clints[db_clint] = db_fun
+#             return db_clints
+#         except Exception as eee:
+#             logger.error('创建用例内数据库连接的错误信息：' + str(eee))
+#             return False
+
+
 def db_clints(db_info):
     if len(db_info) == 0:
         logger.info('用例中未读取到数据库配置信息')
@@ -34,7 +60,7 @@ def db_clints(db_info):
                 db_fun.append(db_info[name]['db_type'].upper())
                 db_connect[name] = db_fun
             except Exception as eee:
-                logger.error('创建abase的错误信息：' + str(eee))
+                logger.error('创建用例内数据库连接的错误信息：' + str(eee))
                 return False
         elif db_info[name]['db_type'].upper() == 'SYBASE':
             try:
@@ -74,8 +100,10 @@ class db_tools(object):
                     cur.execute(str(sql).replace('\n', ''))
                     self.db_clints[db_clint_name][0].commit()
                 except Exception as e:
-                    logger.error("这是个数据库异常" +str(e))
+                    logger.error("这是个数据库异常")
+                    logger.exception(e)
                     self.db_clints[db_clint_name][0].rollback()
+                    raise Exception(e)
                 finally:
                     false_num = false_num + 1
         if false_num != 0:
@@ -92,18 +120,22 @@ class db_tools(object):
                     # sql的dict传入的时候结构为{"db_name(dict_name)": "sql主体"}
                     # 需要对key值再做拆分，并将分离出来的dict_name作为sqlinfo的key值进行补充
                     # 我太难了
-                    cf_dict = re.findall((r'(\w+)'), db_clint_name) #拆分步骤，分解完后是['db_name', 'dict_name']
+                    cf_dict = re.findall(('(\w+)'), db_clint_name) #拆分步骤，分解完后是['db_name', 'dict_name']
                     if len(cf_dict) == 2:
                         # sql = re_fun_for_web.re_fun_sql(sql_dict[db_clint_name], self.keyv)
                         sql = str(base_tool.replace_for_web(sql_dict[db_clint_name], self.keyv)).replace('\\', '')
                         cur = self.db_clints[cf_dict[0]][1]
                         cur.execute(sql)
                         r = cur.fetchall()
+                        r = json.dumps(r, ensure_ascii=False)
+                        r = json.loads(r)
                         '''
                         这里需要判断sql执行的数据库是否为sybase，如果是的话，需要调用转换dict的方法
                         '''
                         if self.db_clints[cf_dict[0]][2] == 'SYBASE':
                             r = base_tool.row_name(sql, r)
+                        else:
+                            r = base_tool.lower2upper(sql, r)
                         # 如果查询结果为多条,则直接返回多条的list,如果有指定的key名,则封装到key内
                         # 这样的话,就没办法处理list的合并,所以,如果要匹配多条记录,则必须在一条sql内查询出所有的验证字段
                         if isinstance(r, list) and len(r) != 1:
@@ -121,6 +153,11 @@ class db_tools(object):
                         cur = self.db_clints[db_clint_name][1]
                         cur.execute(sql)
                         r = cur.fetchall()
+                        r = json.dumps(r, ensure_ascii=False)
+                        r = json.loads(r)
+                        logger.info('JSON后的查询结果：' + str(r))
+                        r = base_tool.lower2upper(sql, r)
+                        logger.info('更新后查询结果：' + str(r) + '类型：' + str(type(r)))
                         sqlinfo = {**sqlinfo, **r[0]}
                     else:
                         return False
