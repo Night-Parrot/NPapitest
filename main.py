6cf3bebf9e3e03c48de93b5985f4c32d1eaa6a97
Night-Parrot <youdewo@163.com>
2020-04-19 18:18:56 +0800

    添加缓存和登录相关模块

--- main.py
+++ main.py
@@ -1,30 +1,62 @@
 import os
 import sys
 import asyncio
-from flask import Blueprint, Flask
+import datetime
+from flask import Blueprint, Flask, jsonify
+from flask_login import LoginManager 
+from db_mod import db_connect
 
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
 from flask_cors import CORS
 
-from db_mod import db_connect
 all_dbc = db_connect.My_dbc()
-from web import main
-from web import JSONEncoder
-from common import my_log
+
+from flask_caching import Cache
+# 简单缓存
+cache_local = Cache(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 60*60*24000000})
+
+
 o_path = os.getcwd()
 sys.path.append(o_path)
+from web import main
+from web import JSONEncoder
 
+
+
+
+from common import login_tool, my_log, cache_tool
 logger = my_log.LogUtil().getLogger()
 
 
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    curr_user = login_tool.User()
+    curr_user.id = user_id
+    return curr_user
+
+
+
+app.secret_key = 'EVANGELION'
+login_manager.login_message = '请登录'
+login_manager.session_protection = 'strong'
+login_manager.init_app(app)
+
+
+
+
+
 CORS(app, supports_credentials=True)
-# app.register_blueprint(main, url_prefix='/apitest')
 app.register_blueprint(main)
+app.register_blueprint(main, url_prefix='/apitest')  # 生产配置
 app.json_encoder = JSONEncoder.MyJSONEncoder
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'upload')
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    hours=1)  # 登录超时的时间设置
 
 
 app.config['ALLOWED_EXTENSIONS'] = ['.xlsx']
@@ -32,7 +64,8 @@ app.config['MAX_CONTENT_LENGTH'] = 1024*1024*60
 
 
 
+
 if __name__ == '__main__':
-    app.run(host="172.18.49.18", port=8585, debug=True)
+    app.run(host="172.18.48.41", port=8585, debug=False,  use_reloader=False)
     # app.run()
 
